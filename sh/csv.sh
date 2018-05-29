b9dd836939d19eecba0296afc9f949de2a16677d
HanDaWoon <bookpia.han@gmail.com>
2018-05-29 19:26:57 +0900

    openSW_Report_sh

---
+++ sh/csv.sh
@@ -0,0 +1,22 @@
+IFS=","
+temp=0
+avertmp=0
+tmp=""
+while read name korean english math
+do
+  sum=`expr $korean + $english + $math`
+  average=`expr $sum / 3`
+  if [ $avertmp -le $average ]
+  then
+    avertmp=$average
+    echo $name","$average > tmp
+  fi
+  if [ $average -ge 80 ] && [ $average -le 89 ]
+  then
+    ((temp = temp + 1))
+  fi
+done < $1
+read tname tavg < tmp
+echo "가장 높은 평균 점수의 학생은 $tname 이고, 점수는 $tavg 입니다."
+echo "B 학점 학생 수는 총 $temp 명 입니다."
+rm tmp
