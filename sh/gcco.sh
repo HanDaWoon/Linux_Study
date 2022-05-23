77ef8474b4dfe9780d31eb33146baaead61969b0
HanDaWoon <bookpia.han@gmail.com>
2022-05-23 18:19:02 +0900

    gcco & wget

--- sh/gcco.sh
+++ sh/gcco.sh
@@ -1,9 +1,12 @@
 function gcco {
-        target=$1
-        if [ $2 ]
-        then
-                gcc -o ${target%.c} $target && ./${target%.c} $2 && rm ${target%.c}
-        else
-                gcc -o ${target%.c} $target && ./${target%.c} && rm ${target%.c}
-        fi
+    target=$1
+    if [ $2 ]
+    then
+        gcc -o ${target%.c} $target && ./${target%.c} $2 && rm ${target%.c}
+    elif [ $3 ]
+    then
+        gcc -o ${target%.c} $target && ./${target%.c} $2 $3 && rm ${target%.c}
+    else
+        gcc -o ${target%.c} $target && ./${target%.c} && rm ${target%.c}
+    fi
 }
